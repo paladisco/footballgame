591aca8a8cb68e1e22cd59e6efc268726c577a7d
diegopaladino <paladino@racerfish.com>
2014-03-30 16:04:04 +0200

    Cleaned Public Directory and added Bower components, adjustments for SASS integration CSS

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+// ----- General reusable function for ajax calls -----
+function ajaxCall(url,target,callbackFunction) {
+    $.ajax({
+        url: url,
+        cache: false,
+        spinnerTarget: target
+    }).done(function( html ) {
+            callbackFunction(html);
+        });
+}
+
+$(document).ready(function(){
+    $('.modal').on('hidden.bs.modal', function() {
+        $(this).removeData('bs.modal');
+        $(this).html('')
+    });
+});
+
